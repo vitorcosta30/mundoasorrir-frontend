6df05420da2ad9927d6936c5ea1c13689522e2ea
1201391 <1201391@isep.ipp.pt>
2023-11-07 19:40:22 +0000

    Update to auth service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -41,4 +41,7 @@ export class AuthService {
   logout(): Observable<any> {
     return this.http.post(AUTH_API + 'signout', { }, httpOptions);
   }
+  refreshToken() {
+    return this.http.post(AUTH_API + 'refreshtoken', { }, httpOptions);
+  }
 }
