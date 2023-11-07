3933459b53bce9278071bb267496efce8319e508
1201391 <1201391@isep.ipp.pt>
2023-11-07 19:48:35 +0000

    Http interceptor

---
+++ src/app/interceptor/http.interceptor.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
+
+import { StorageService } from '../services/storage/storage.service';
+import { SystemEventService } from '../systemEvent/systemEvent.service';
+import { SystemEvent } from '../systemEvent/systemEvent.class';
+import { AuthService } from '../services/auth/auth.service';
+ const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
+
+@Injectable()
+export class HttpRequestInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
+
+  constructor(private storageService: StorageService,   private authService: AuthService,private eventBusService: SystemEventService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      withCredentials: true,
+    });
+
+    return next.handle(req).pipe(
+      catchError((error) => {
+        if (
+          error instanceof HttpErrorResponse &&
+          !req.url.includes('auth/signin') &&
+          error.status === 401
+        ) {
+          return this.handle401Error(req, next);
+        }
+
+        return throwError(() => error);
+      })
+    );
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+
+      if (this.storageService.isLoggedIn()) {
+        return this.authService.refreshToken().pipe(
+          switchMap(() => {
+            this.isRefreshing = false;
+
+            return next.handle(request);
+          }),
+          catchError((error) => {
+            this.isRefreshing = false;
+
+            if (error.status == '403') {
+              this.eventBusService.emit(new SystemEvent('logout', null));
+            }
+
+            return throwError(() => error);
+          })
+        );
+      }
+    }
+
+    return next.handle(request);
+  }
+}
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
+];
