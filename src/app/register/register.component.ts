63d8708766618780b045eb353bbd5ea48c0636aa
1201391 <1201391@isep.ipp.pt>
2023-11-24 12:18:57 +0000

    update to register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { Role } from './role.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   form: any = {
     username: null,
     email: null,
@@ -16,8 +18,31 @@ export class RegisterComponent {
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
+  roles : Role[] = [];
+  roleNameDropdownSettings: any;
+
 
   constructor(private authService: AuthService) { }
+  
+  
+  ngOnInit(): void {
+    this.getRoles();
+
+
+  }
+  initDropdownSettings() {
+    this.roleNameDropdownSettings = {
+      singleSelection: true,
+      idField: 'id',
+      textField: 'name',
+      itemsShowLimit: 10,
+      allowSearchFilter: true,
+      maxHeight: '200'
+    };
+  }
+  getRoles(): void {
+    this.authService.getRoles().subscribe(r => this.roles = r);
+  }
 
   onSubmit(): void {
     const { username, email, password, role } = this.form;
