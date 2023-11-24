95f37f1ac644e443803d110479cc718aa5b1ac89
1201391 <1201391@isep.ipp.pt>
2023-11-24 12:21:24 +0000

    Vacation service

---
+++ src/app/services/vacation/vacation.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+const VACATION_API = 'http://localhost:9000/api/vacation/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VacationService {
+  constructor(private http: HttpClient) {}
+
+
+
+  createRequest(startDate: string,endDate: string): Observable<any>{
+    const formData: FormData = new FormData();
+    formData.append('startDate',startDate);
+    formData.append('endDate',endDate);
+
+
+    return this.http.post<any>(VACATION_API  + 'createRequest', formData);
+
+  }
+
+  rejectRequest(idRequest:string): Observable<any>{
+    const formData: FormData = new FormData();
+    formData.append('requestId',idRequest);
+    return this.http.post<any>(VACATION_API  + 'rejectRequest', formData);
+
+  }
+  acceptRequest(idRequest:string): Observable<any>{
+    const formData: FormData = new FormData();
+    formData.append('requestId',idRequest);
+    return this.http.post<any>(VACATION_API  + 'acceptRequest', formData);
+
+  }
+
+  getRequestDate(dateString: string): Observable<any>{7
+    const formData: FormData = new FormData();
+    formData.append('obsDate',dateString);
+    return this.http.post<any>(VACATION_API  + 'getActiveVacations', formData);
+
+  }
+  getRequestToday(): Observable<any>{
+
+    return this.http.get<any>(VACATION_API  + 'getActiveVacationsToday').pipe();
+
+  }
+
+
+
+
+  getPendingRequests(): Observable<any> {
+    return this.http.get(VACATION_API + 'getPendingRequests', { }).pipe();
+  }
+
+
+
+
+}
