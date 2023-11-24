3599f3296f430de0fbb31b95a1a5f022870555e6
1201391 <1201391@isep.ipp.pt>
2023-11-24 12:20:50 +0000

    file display component

---
+++ src/app/file-display/file-display.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from '../services/file-upload/file-upload.service';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-file-display',
+  templateUrl: './file-display.component.html',
+  styleUrls: ['./file-display.component.css']
+})
+export class FileDisplayComponent implements OnInit {
+  id?: number;
+  url: string = "";
+  uri: any;
+
+
+  constructor(private fileUploadService: FileUploadService,    private activatedRoute: ActivatedRoute , private sanitizer:DomSanitizer   ){
+
+  }
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+      this.url = "http://localhost:9000/api/files/files/"+this.id;
+      this.uri = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+;
+    })
+  }
+
+}
