cc8d7ee299f83c3537b34c1c57f7899c22d209a9
1201391 <1201391@isep.ipp.pt>
2023-11-24 12:14:09 +0000

    Vacation component

---
+++ src/app/vacation/vacation.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { VacationService } from '../services/vacation/vacation.service';
+@Component({
+  selector: 'app-vacation',
+  templateUrl: './vacation.component.html',
+  styleUrls: ['./vacation.component.css']
+})
+export class VacationComponent {
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+  message = '';
+
+
+  startDate = new Date();
+  endDate = new Date();
+  eventType = '';
+  users: string[] = [];
+
+  constructor(private vacationService: VacationService) { }
+
+
+
+
+
+
+  onSubmit(): void {
+
+    this.vacationService.createRequest(this.startDate.toString(), this.endDate.toString()).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+        this.message = data.message;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
+
+
+}
