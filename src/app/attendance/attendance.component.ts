73acb5a57432e4030642ab6dfbdd4acc9ee80e0a
1201391 <1201391@isep.ipp.pt>
2023-11-26 23:23:41 +0000

    Update to attendance component

--- src/app/attendance/attendance.component.ts
+++ src/app/attendance/attendance.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../user/user.model';
+import { AttendanceService } from '../services/attendance/attendance.service';
 
 @Component({
   selector: 'app-attendance',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./attendance.component.css']
 })
 export class AttendanceComponent {
+  usersInDay : User [] = [];
+  date = new Date();
+
+
+  constructor(private vacationService :  AttendanceService){
+  }
+  ngOnInit(): void {
+    this.getUnmarkedPresences();
+  }
+
+  getUnmarkedPresences(): void {
+    this.vacationService.getUnmarkedRequest(this.date.toString()).subscribe(res => this.usersInDay =  res);
+  }
+  isPresent(username: string): void{
+    this.vacationService.isPresent(this.date.toString(),username).subscribe(res =>{
+      this.getUnmarkedPresences();
+    }
+      );
+
+  }
+  isAbsent(username: string): void{
+    this.vacationService.isAbsent(this.date.toString(),username).subscribe(res =>{
+      this.getUnmarkedPresences();
+    }
+      );
+    
+  }
+
+
 
 }
