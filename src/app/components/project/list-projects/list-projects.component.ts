f54dec6b5bd5b5873cf972c25c6f12db79e303a0
1201391 <1201391@isep.ipp.pt>
2023-12-16 17:30:56 +0000

    Implementation of listing projects funcionality

--- src/app/components/project/list-projects/list-projects.component.ts
+++ src/app/components/project/list-projects/list-projects.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project.model';
+import { ProjectService } from 'src/app/services/project/project.service';
+import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
   selector: 'app-list-projects',
   templateUrl: './list-projects.component.html',
   styleUrls: ['./list-projects.component.css']
 })
-export class ListProjectsComponent {
+export class ListProjectsComponent implements OnInit{
+  projects: Project[] = []
+
+
+  constructor(private projectService: ProjectService,  private storageService: StorageService){}
+  ngOnInit(): void {
+    this.getProjects();
+  }
+  isDirector(): boolean{
+    return this.storageService.isDirector();
+  }
+  activate(id : number): void{
+    this.projectService.activateProject(id).subscribe(res =>{
+      this.getProjects()
+    })
+
+  }
+  deactivate(id : number): void{
+    this.projectService.deactivateProject(id).subscribe(res =>{
+      this.getProjects()
+    })
+  }
+
+  getProjects(): void {
+    this.projectService.getAllProjects().subscribe(res => this.projects = res);
+
+  }
+
 
 }
